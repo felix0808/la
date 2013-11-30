a3ec124119da277b1b198e730ee43c264724844e
felix <felix@tryme.(none)>
2013-11-30 21:51:08 +0400

    III.2 touch settings.conf print_settings.sh chmod u+x print_settings.sh chmod g-w settings.conf

---
+++ session_3/print_settings.sh
@@ -0,0 +1,6 @@
+#1/bin/bash
+. settings.conf
+echo "########################################
+## Default login/password: $OS_USERNAME/$OS_PASSWORD
+## tennant: $OS_TENANT_NAME
+########################################"
