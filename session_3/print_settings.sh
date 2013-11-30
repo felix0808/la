b77626cce29984dff609c8d0c42dc4cc11a8708e
felix <felix@tryme.(none)>
2013-11-30 22:11:08 +0400

    III.2 update

--- session_3/print_settings.sh
+++ session_3/print_settings.sh
@@ -1,6 +1,13 @@
 #1/bin/bash
 . settings.conf
-echo "########################################
-## Default login/password: $OS_USERNAME/$OS_PASSWORD
-## tennant: $OS_TENANT_NAME
-########################################"
+
+function generateGrid(){
+	for i in {1..40};do
+		echo -n "#"
+	done
+}
+generateGrid 
+echo -e "\n## Default login/password: $OS_USERNAME/$OS_PASSWORD
+## tennant: $OS_TENANT_NAME"
+generateGrid
+
