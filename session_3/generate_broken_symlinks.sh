c4c13cbd848e31ff05b5bae2c70de55222dc93e0
felix <felix@tryme.(none)>
2013-12-01 00:24:54 +0400

    III.3.b

--- session_3/generate_broken_symlinks.sh
+++ session_3/generate_broken_symlinks.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 touch not_ver.file
-i=0
-while [ $i -le 10 ];do
-	touch "ver$i.file"
-	ln -s "ver$i.file" "ver$i.file.lnk"
-	i=$(expr $i + 1)
-done
 
-rm -f ver*
+if [ $# -eq 0 ];then
+	for i in {0..10};do
+		touch "ver$i.file"
+		ln -s "ver$i.file" "ver$i.file.lnk"
+	done
+	rm -f ver*
+elif [ $# -eq 1 ];then
+	directory="$1"
+	mkdir -p $directory 2> /dev/null
+	if [ $? -ne 0 ];then
+		echo "Cant create folder :("
+		exit
+	fi
+	for i in {0..10};do
+		touch "$directory""ver$i.file"
+		ln -s "$directory""ver$i.file" "$directory""ver$i.file.lnk"
+	done
+	rm -f "$directory"ver*
+else echo "Sorry, but this script not support 2 or more arguments"
+fi
